c3148103c6486d1080fb655087cd3f3b1681f3a0
gonzalez-7 <20agonzalezp@gmail.com>
2024-10-23 19:09:45 -0700

    Answers

--- script.js
+++ script.js
@@ -10,7 +10,21 @@
 */
 console.log("Hello World");
 
-console.log(8 * null); //result: 9
+console.log(8 * null); //result: 0
 
-console.log(10 % 2 == 0 ? "even" : "odd");
+console.log("5" - 1); // result 4
+
+console.log("5" + 1); //result "51"
+
+console.log("five" * 2); //result: NoN
+
+console.log(false == 0); //result: true
+
+console.log(5 > 2 && 6 > 3); //result: true
+
+console.log(false == false ? "equal" : "not equal"); //result: "equal"
+
+console.log(true || false); //result: true
+
+console.log(10 % 2 == 0 ? "even" : "odd"); //result: "even"
 
