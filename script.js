9f0d56c9ccb713a170b17cc79eb51a4a92d5649a
gonzalez-7 <20agonzalezp@gmail.com>
2024-10-23 19:08:59 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+//this is a comment line
+/*
+    This 
+    is
+    a
+    a
+    Multi-line
+    comment
+
+*/
+console.log("Hello World");
+
+console.log(8 * null); //result: 9
+
+console.log(10 % 2 == 0 ? "even" : "odd");
+
