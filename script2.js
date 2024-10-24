7d7fb392190fc7dbfe386decbb7ca7790067be69
gonzalez-7 <20agonzalezp@gmail.com>
2024-10-23 19:58:43 -0700

    Activity 2

---
+++ script2.js
@@ -0,0 +1,25 @@
+// Define a variable "myGreeting"
+let myGreeting = "Hello World";
+console.log(myGreeting); // Output the contents of “myGreeting”
+
+myGreeting = "Hola";
+console.log(myGreeting);
+
+
+// Define a constant variable "pi"
+const pi = 3.14159;
+
+// Attempt to re-assign "pi"
+
+pi = 3.14; // This will throw an error
+
+// Define a new variable "myNumber"
+let myNumber = 10;
+
+// Call the “min” function on both “myNumber” and pi
+let minValue = Math.min(myNumber, pi);
+console.log("The minimum value is:", minValue); // Log the result
+
+
+
+
